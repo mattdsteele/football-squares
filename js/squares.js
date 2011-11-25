3a57a2b2ed15456a2ed24e161da79a37f4b14db8
Matt Steele <orphum@gmail.com>
2011-11-24 22:07:13 -0600

    Adding in some basic color-coding

--- js/squares.js
+++ js/squares.js
@@ -1,10 +1,17 @@
 function getSquare(home, away) {
   return $('table tbody td')[(home * 11) + (away + 1)];
 }
+
+function makeBackground(square, outcome) {
+  square.css('background-color', "#" + ((255 - Number(Math.round(outcome * 50.0))).toString(16) + "ffff"));
+}
+
 $(function() {
   $.get('/scores.json', function(data) {
     $.each(data, function(index, value) {
-      $(getSquare(value.home, value.away)).html(value.outcome);
+      square = $(getSquare(value.home, value.away));
+      square.html(value.outcome);
+      makeBackground(square, value.outcome);
     });
   });
 });
