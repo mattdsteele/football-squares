0cf79997e93f28dbf2dd31045d2a87947ce94e54
Matt Steele <orphum@gmail.com>
2011-11-25 00:14:58 -0600

    made more tweaks to the color-coding

--- js/squares.js
+++ js/squares.js
@@ -2,16 +2,36 @@ function getSquare(home, away) {
   return $('table tbody td')[(home * 11) + (away + 1)];
 }
 
-function makeBackground(square, outcome) {
-  square.css('background-color', "#" + ((255 - Number(Math.round(outcome * 50.0))).toString(16) + "ffff"));
+var colors = ["CCFFFF", "52FFFF", "BF00FF", "FF00BF", "FF0040", "FF4000"]
+
+function makeBackground(square, outcome, stats) {
+  var index = Math.round((outcome - stats.min) / stats.max * (colors.length - 1));
+  square.css('background-color', "#" + colors[index]);
+  square.attr('data-outcome', outcome);
+  square.mouseover(function() {
+    $(this).html($(this).attr('data-outcome'));
+  });
+  square.mouseleave(function() { $(this).empty(); });
+}
+
+function getMinAndMax(data) {
+  var min = Number.MAX_VALUE;
+  var max = 0;
+  $.each(data, function(i, v) {
+    if (v.outcome < min) { min = v.outcome; }
+    if (v.outcome > max) { max = v.outcome; }
+  });
+  var deltas = 6;
+  var diff = (max - min) / deltas;
+  return {"min" : min, "max" : max, "diff" : diff, "deltas" : deltas};
 }
 
 $(function() {
   $.get('/scores.json', function(data) {
+    var stats = getMinAndMax(data);
     $.each(data, function(index, value) {
-      square = $(getSquare(value.home, value.away));
-      square.html(value.outcome);
-      makeBackground(square, value.outcome);
+      var square = $(getSquare(value.home, value.away));
+      makeBackground(square, value.outcome, stats);
     });
   });
 });
