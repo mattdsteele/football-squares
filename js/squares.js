15f4b510a1bf2cdd4c65212c0780cb9b4825f9dd
Matt Steele <orphum@gmail.com>
2011-11-25 15:58:04 -0600

    Changed form values to <input>

--- js/squares.js
+++ js/squares.js
@@ -1,3 +1,14 @@
+jQuery.fn.compare = function(t) {
+  if (this.length != t.length) { return false; }
+  var a = this.sort(), b = t.sort();
+  for (var i = 0; i < t.length; i++) {
+    if (a[i] != b[i]) { 
+      return false;
+    }
+  }
+  return true;
+};
+
 function getSquare(home, away) {
   return $('table tbody td')[(home * 11) + (away + 1)];
 }
@@ -26,6 +37,18 @@ function getMinAndMax(data) {
   return {"min" : min, "max" : max, "diff" : diff, "deltas" : deltas};
 }
 var populate = function() {
+  var homeScores = [];
+  $('thead td input').each(function(i,j) { homeScores.push($(j).val()); });
+  if (!$(homeScores).compare([1,2,3,4,5,6,7,8,9,0])) {
+    alert("Please enter valid values!");
+    return false;
+  }
+  homeScores = [];
+  $('tbody td input').each(function(i,j) { homeScores.push($(j).val()); });
+  if (!$(homeScores).compare([1,2,3,4,5,6,7,8,9,0])) {
+    alert("Please enter valid values!");
+    return false;
+  }
   $.get('/scores.json', function(data) {
     var stats = getMinAndMax(data);
     $.each(data, function(index, value) {
@@ -33,6 +56,7 @@ var populate = function() {
       makeBackground(square, value.outcome, stats);
     });
   });
+  return false;
 };
 
 $(function() {
