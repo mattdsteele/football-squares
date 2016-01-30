2ab4a8560b8cc2f88d5f0b80b725ca9737dd382f
Matt Steele <orphum@gmail.com>
2016-01-30 16:08:43 -0600

    Upgrade Datasets service to Angular 2; downgrade to use in ng1 code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 import angular from 'angular';
 
-import datasets from './datasets';
+import Datasets from './datasets';
 import Data from './data';
 import squares from './squares';
 import squareCell from './square-cell';
 
+import upgradeAdapter from './adapter.js';
+upgradeAdapter.addProvider(Datasets);
+
 let app = angular.module('squares', [])
-  .service('Datasets', datasets)
   .service('Data', Data)
+  .factory('Datasets', upgradeAdapter.downgradeNg2Provider(Datasets))
   .component('squareCell', squareCell)
   .component('superbowlSquares', squares);
 
--- js/datasets.js
+++ js/datasets.js
@@ -1,3 +1,6 @@
+import { Injectable } from 'angular2/core';
+
+@Injectable()
 class Datasets {
   constructor() {
     this.datasets = [
