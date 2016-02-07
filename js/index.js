faaaf6b2098e1fc5de0f6a3612cec7e0e1594dda
Matt Steele <orphum@gmail.com>
2016-02-07 07:56:29 -0600

    Test out 'production mode'

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone-microtask';
 
 import { bootstrap } from 'angular2/platform/browser';
+import { enableProdMode } from 'angular2/core';
 
 import SuperbowlApp from './superbowl-app';
 
+//enableProdMode();
 bootstrap(SuperbowlApp);
 
