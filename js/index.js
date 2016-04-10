813111400375e09ad1fed34ed4b6442ca5a7c913
Matt Steele <orphum@gmail.com>
2016-04-10 15:32:50 -0500

    Update Angular to (almost) current version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import 'zone.js/dist/zone-microtask';
+import 'zone.js';
 
 import { bootstrap } from 'angular2/platform/browser';
 import { enableProdMode } from 'angular2/core';
