bef1f44f3b3fefe8bd10179e720d89b1e9f790a2
Matt Steele <orphum@gmail.com>
2016-04-17 09:43:04 -0500

    test: Add test for header

---
+++ src/header.spec.js
@@ -0,0 +1,21 @@
+import './header';
+
+describe('square cell', () => {
+  let el;
+  beforeEach(() => {
+    angular.mock.module('header');
+  });
+  beforeEach(inject(($compile, $rootScope) => {
+    el = $compile(`<superbowl-header>
+                    <lede>A sample lede</lede>
+                    <subhead>A sub header</subhead>
+                  </superbowl-header>`)($rootScope.$new());
+  }));
+  it('sets the lede', () => {
+    expect(el[0].querySelector('.header-1').textContent).toBe('A sample lede');
+  });
+
+  it('sets the subhead', () => {
+    expect(el[0].querySelector('.header-2').textContent).toBe('A sub header');
+  });
+});
