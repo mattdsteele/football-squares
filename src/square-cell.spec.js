cd055f3a0a8428a684548c375f69198d76ff887b
Matt Steele <orphum@gmail.com>
2016-04-17 09:35:38 -0500

    test: more tests for square cell

--- src/square-cell.spec.js
+++ src/square-cell.spec.js
@@ -44,4 +44,35 @@ describe('square cell', () => {
       expect(squares.priorityLevel).toBe('priority-level-2');
     });
   });
+
+  describe('component', () => {
+    let el;
+    beforeEach(inject(($compile, $rootScope) => {
+      scope = $rootScope.$new();
+      scope.stats = { min: 0, max: 12, diff: 2, deltas: 6 };
+
+      el = $compile('<square-cell home="0" away="3" stats="stats" dataset="superbowl" always-visible="alwaysVisible">')(scope);
+
+      scope.superbowl = superbowl;
+      scope.alwaysVisible = false;
+      scope.$digest();
+    }));
+
+    it('is triggered on mouse events', () => {
+      el.find('div').triggerHandler('mouseenter');
+      scope.$digest();
+      expect(el.text()).toBe('2.22');
+
+      el.find('div').triggerHandler('mouseleave');
+      scope.$digest();
+      expect(el.text()).toBe('');
+    });
+
+    it('shows when told to always show', () => {
+      scope.alwaysVisible = true;
+      scope.$digest();
+
+      expect(el.text()).toBe('2.22');
+    });
+  });
 });
