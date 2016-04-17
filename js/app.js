95377ae98eaef377bef74194af272de3710349cf
Matt Steele <orphum@gmail.com>
2016-04-16 20:41:40 -0500

    Add eslint

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ import Data from './data';
 import squares from './squares';
 import squareCell from './square-cell';
 
-let app = angular.module('squares', [])
+const app = angular.module('squares', [])
   .service('Datasets', datasets)
   .service('Data', Data)
   .component('squareCell', squareCell)
