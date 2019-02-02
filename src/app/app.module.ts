223e675328b0d1500c195053039097c6e04e035e
mattdsteele <orphum@gmail.com>
2019-02-02 16:24:23 -0600

    PWAify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FootballSquaresModule } from './football-squares/football-squares.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 // let imports: any[] = [FootballSquaresModule, BrowserModule];
 // if (environment.production) {
@@ -9,7 +11,7 @@ import { FootballSquaresModule } from './football-squares/football-squares.modul
 // }
 @NgModule({
   declarations: [AppComponent],
-  imports: [FootballSquaresModule, BrowserModule],
+  imports: [FootballSquaresModule, BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
